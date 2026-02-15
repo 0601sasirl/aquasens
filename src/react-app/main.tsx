1e7ef92343f75ccb64d158be6fbc245e3c9cc30a
Sakshi dhaka <sakshi062btece25@igdtuw.ac.in>
2026-02-16 02:18:31 +0530

    fix: complete tailwind and postcss configuration

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./react-app/index.css";  // ← Make sure this line is here
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
